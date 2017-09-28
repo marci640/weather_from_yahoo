8e8a309b44a7231cfb2c313f9d124ad1ee0b7a5d
marci <marcifey@gmail.com>
2017-09-27 17:18:33 -0700

    api wrapper instead of unirest

--- app/controllers/temps_controller.rb
+++ app/controllers/temps_controller.rb
@@ -1,20 +1,28 @@
 class TempsController < ApplicationController
+  
   def index
-
-    if params[:city] && params[:state]
-      city = params[:city]
-      state = params[:state] 
-    else 
-      city = "sanfrancisco"
-      state = "ca"
-    end 
-
-    @temps = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22#{city}%2C%20#{state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
-
-    @current_temp = @temps['query']['results']['channel']['item']['condition']['temp']
-    
-    @forecast = @temps['query']['results']['channel']['item']['forecast']
-    
-    @city_name = @temps['query']['results']['channel']['location']['city']
+   
+  if params[:woeid]
+    @woeid = params[:woeid]
+  else 
+    @woeid = 2487956
   end 
-end
+
+   @response = Weather.lookup(@woeid, Weather::Units::FAHRENHEIT)
+
+   @title = @response.title
+   @temp = @response.condition.temp 
+   @condition = @response.condition.text
+   @image = @response.image.url
+
+   if @condition == "Sunny"
+    @background = "yellow"
+   elsif @condition == "Partly Cloudy"
+    @background = "powderblue"
+   else    
+    @background = "grey"
+   end 
+   
+ end
+end 
+
